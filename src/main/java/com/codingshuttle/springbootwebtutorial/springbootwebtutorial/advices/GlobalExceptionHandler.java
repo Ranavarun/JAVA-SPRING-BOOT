ab58525e8c9f694e8dd33ca92d81293cdca0b8d8
varun rana <varun1sep2002@gmail.com>
2025-12-25 12:54:02 +0530

    Revert "Chapter 2.8 Transforming API Response"
    This reverts commit 45c4c5cb63c2765fc5897d4838b34aea07948add.

--- src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
@@ -8,30 +8,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception) {
+    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
                 .build();
-        return buildErrorResponseEntity(apiError);
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception) {
+    public ResponseEntity<ApiError> handleInternalServerError(Exception exception) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(exception.getMessage())
                 .build();
-        return buildErrorResponseEntity(apiError);
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<?>> handleInputValidationErrors(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception) {
         List<String> errors = exception
                 .getBindingResult()
                 .getAllErrors()
@@ -44,11 +46,7 @@ public class GlobalExceptionHandler {
                 .message("Input validation failed")
                 .subErrors(errors)
                 .build();
-        return buildErrorResponseEntity(apiError);
-    }
-
-    private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
-        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
 }
